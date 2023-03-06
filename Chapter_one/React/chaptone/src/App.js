3ad1bc499f73280b0de26940c38a934d736ffa12
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2023-03-06 09:48:27 -0300

    Component

---
+++ Chapter_one/React/chaptone/src/App.js
@@ -0,0 +1,12 @@
+import { RepositoryList } from './components/RepositoryList';
+import "./Styles/global.css"
+
+function App() {
+  return (
+    <>
+      <RepositoryList/>
+    </>
+  );
+}
+
+export default App;
