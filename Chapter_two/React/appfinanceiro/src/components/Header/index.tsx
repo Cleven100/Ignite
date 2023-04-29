904a330a50b6d9d9416598fa91b5553e7b4e4b3a
cleven100 <cleven100_100@hotmail.com>
2023-04-29 12:59:28 -0300

    29/04

--- Chapter_two/React/appfinanceiro/src/components/Header/index.tsx
+++ Chapter_two/React/appfinanceiro/src/components/Header/index.tsx
@@ -1,16 +1,34 @@
+import { useState } from 'react'
+import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
 
 
 export function Header() {
+
+  const [newTransactionModalOpen, setNewTransactionModalOpen] = useState(false);
+
+ function handleOpenNewTransactionModal() {
+    setNewTransactionModalOpen(true);
+ }
+
+ function handleCloseNewTransactionModal() {
+  setNewTransactionModalOpen(false);
+ }
+
     return(
         <Container>
             <Content>
                  <img src={logoImg} alt="dt money"></img>
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModal}>
                 Nova transação
                </button>
+
+
+               <Modal isOpen={newTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
+                 <h2>Cadastrar transação</h2>
+               </Modal>
             </Content>
            
         </Container>
